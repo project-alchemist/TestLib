ad46024834126d941155ac1dde39e04a1a86aea3
project-alchemist <kai.rothauge@berkeley.edu>
2019-01-16 13:34:23 -0800

    Fixed typos in config file

--- config.sh
+++ config.sh
@@ -7,16 +7,14 @@ if [ "$SYSTEM" = "" ]; then
   export SYSTEM="Linux"
 fi
 
-if [ "$SYSTEM" == "MacOS" ]
-then
+if [ "$SYSTEM" == "MacOS" ]; then
 	export TESTLIB_PATH=$HOME/Projects/AlLib
 	
 	export ELEMENTAL_PATH=$HOME/Software/Elemental
 	export EIGEN3_PATH=$HOME/Software/Eigen3
 	export ARPACK_PATH=$HOME/Software/ARPACK
 	
-elif [ "$SYSTEM" == "Cori" ]
-then
+elif [ "$SYSTEM" == "Cori" ]; then
 	export TESTLIB_PATH=$SCRATCH/Projects/TestLib
 	
 	export ELEMENTAL_PATH=$SCRATCH/Software/Elemental
@@ -24,8 +22,7 @@ then
 	export EIGEN3_PATH=$SCRATCH/Software/Eigen3
 	export ARPACK_PATH=$SCRATCH/Software/ARPACK
 	
-elif [ "$SYSTEM" == "Linux" ]
-then
+elif [ "$SYSTEM" == "Linux" ]; then
 	export TESTLIB_PATH=/usr/local/TestLib
 	
 	export ELEMENTAL_PATH=/usr/local/elemental
