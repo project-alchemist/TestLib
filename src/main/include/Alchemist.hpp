f99199dc5375da2d9fc22ea67d4718355b00b6f0
project-alchemist <kai.rothauge@berkeley.edu>
2019-01-17 18:44:15 -0800

    Debugging Linux dl

--- src/main/include/Alchemist.hpp
+++ src/main/include/Alchemist.hpp
@@ -1594,25 +1594,25 @@ typedef std::shared_ptr<Library> Library_ptr;
 
 #pragma once
 
-#ifdef  __cplusplus
+//#ifdef  __cplusplus
 extern "C" {
-#endif
+//#endif
 
 void * create(MPI_Comm &);
 
-#ifdef  __cplusplus
+//#ifdef  __cplusplus
 }
-#endif
+//#endif
 
-#ifdef  __cplusplus
+//#ifdef  __cplusplus
 extern "C" {
-#endif
+//#endif
 
 void destroy(void *);
 
-#ifdef  __cplusplus
+//#ifdef  __cplusplus
 }
-#endif
+//#endif
 
 
 }
