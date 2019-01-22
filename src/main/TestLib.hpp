96a22950b0ee629e41532753cabeca3099715491
project-alchemist <kai.rothauge@berkeley.edu>
2019-01-21 18:09:14 -0800

    Debugging Linux dl

---
+++ src/main/TestLib.hpp
@@ -0,0 +1,64 @@
+#ifndef TESTLIB_HPP
+#define TESTLIB_HPP
+
+//#include <omp.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <poll.h>
+#include <thread>
+#include <El.hpp>
+#include <algorithm>
+#include <random>
+#include <cmath>
+#include <chrono>
+#include <eigen3/Eigen/Dense>
+#include "arpackpp/arrssym.h"
+//#include <cmath>
+//#include <chrono>
+//#include <cassert>
+//#include <cstdlib>
+//#include <cstdio>
+//#include <memory>
+//#include <unistd.h>
+//#include <arpa/inet.h>
+#include "include/Alchemist.hpp"
+//#include "nla/nla.hpp"						// Include all NLA routines
+//#include "ml/ml.hpp"							// Include all ML/Data-mining routines
+
+namespace alchemist {
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+struct TestLib : Library {
+
+	TestLib(MPI_Comm & world);
+
+	~TestLib() { }
+
+	Log_ptr log;
+
+	int world_rank;
+
+	int load();
+	int unload();
+
+	int run(string & name, alchemist::Parameters & input, alchemist::Parameters & output);
+};
+
+// Class factories
+extern "C" {
+	void * create_library(MPI_Comm & world) {
+		return reinterpret_cast<void*>(new TestLib(world));
+	}
+
+	void destroy_library(void * p) {
+		delete reinterpret_cast<TestLib*>(p);
+	}
+}
+
+}
+
+#endif // TESTLIB_HPP
